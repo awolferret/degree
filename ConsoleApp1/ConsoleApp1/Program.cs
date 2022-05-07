f582b7e00605d1064a83d1d52c351096ab3fe89e
awolferret <104518827+awolferret@users.noreply.github.com>
2022-05-07 21:50:20 +0300

    2

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int minValue = 0;
+            int maxValue = 101;
+            Random random = new Random();
+            int targetNumber = random.Next(minValue, maxValue);
+            int foundation = 2;
+            int degree = 1;
+
+            for (int i = foundation; i < targetNumber; i *= foundation)
+            {
+                degree++;
+            }
+            Console.WriteLine($"минимальная степень: {degree}");
+        }
+    }
+}
